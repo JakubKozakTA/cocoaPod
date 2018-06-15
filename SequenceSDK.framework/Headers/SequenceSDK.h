33a86607688d7a8628742a3122160fd8c9b4dbdc
Jakub Kozák <jakub.kozak@touchart.cz>
2018-06-15 15:34:25 +0200

    - init

---
+++ SequenceSDK.framework/Headers/SequenceSDK.h
@@ -0,0 +1,19 @@
+//
+//  SequenceSDK.h
+//  SequenceSDK
+//
+//  Created by Jakub Kozák on 14/03/2018.
+//  Copyright © 2018 OAK'S LAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SequenceSDK.
+FOUNDATION_EXPORT double SequenceSDKVersionNumber;
+
+//! Project version string for SequenceSDK.
+FOUNDATION_EXPORT const unsigned char SequenceSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SequenceSDK/PublicHeader.h>
+
+
